7c157abbb952388e9273598ba94cb268b9b6658a
jmarin <josepablomarinvalera@gmail.com>
2019-04-26 20:40:55 +0200

    setting up

---
+++ CV/build/CMakeFiles/project_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_test.dir/main.cpp.obj"
+  "CMakeFiles/project_test.dir/lib/date.cpp.obj"
+  "project_test.pdb"
+  "project_test.exe"
+  "project_test.exe.manifest"
+  "libproject_test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
